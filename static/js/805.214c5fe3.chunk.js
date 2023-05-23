d7a5d670eb2b06a7f61f74e96698f26c9f91bb4c
Yaroslav-Marinich <113300647+Yaroslav-Marinich@users.noreply.github.com>
2023-05-23 12:41:25 +0000

    Deploying to gh-pages from @ Yaroslav-Marinich/goit-react-hw-05-movies@3352037812806130c5e2dc752974e38636322361 🚀

---
+++ static/js/805.214c5fe3.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[805],{805:function(e,t,r){r.r(t);var n=r(87),c=r(184);t.default=function(){return(0,c.jsxs)("div",{children:[(0,c.jsx)("p",{children:"Page not found"}),(0,c.jsx)(n.rU,{to:"/",children:"Back to Home"})]})}}}]);
+//# sourceMappingURL=805.214c5fe3.chunk.js.map
